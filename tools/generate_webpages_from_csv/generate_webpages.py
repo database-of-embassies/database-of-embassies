ee1850acb628c5c28f38b223ff86c4e1c6406de5
Nicolas Raoul <nicolas.raoul@gmail.com>
2021-01-14 19:36:43 +0900

    More properties

--- tools/generate_webpages_from_csv/generate_webpages.py
+++ tools/generate_webpages_from_csv/generate_webpages.py
@@ -1,16 +1,28 @@
 import os
 import sys
+from urllib import unquote
+import hashlib
 
 def safe_for_url(string):
     return string.lower().replace(' ', '_').replace("'", '_')
+
+# Does not work for https://commons.wikimedia.org/wiki/File:%E3%82%B8%E3%83%96%E3%83%81%E5%A4%A7%E4%BD%BF%E9%A4%A8%E3%81%AF%E4%B8%80%E8%BB%92%E5%AE%B6.jpg
+def commons_thumbnail(image, width=300): # image = e.g. from Wikidata, width in pixels
+    image = image[len('http://commons.wikimedia.org/wiki/Special:FilePath/'):]
+    image = unquote(image)
+    image = image.replace(' ', '_') # need to replace spaces with underline 
+    m = hashlib.md5()
+    m.update(image.encode('utf-8'))
+    d = m.hexdigest()
+    return "https://upload.wikimedia.org/wikipedia/commons/thumb/"+d[0]+'/'+d[0:2]+'/'+image+'/'+str(width)+'px-'+image
 
 class DiplomaticRepresentation:
     def __init__(self, csv_row):
         cells = csv_row.rstrip().split(';') # rstrip removes the trainling \n character.
         self.operator = cells[0]
         self.operatorQID = cells[1]
-        self.jurisdictions = cells[2] # TODO split into array
-        self.jurisdictionQIDs = cells[3] # TODO
+        self.jurisdictions = cells[2].split('|')
+        self.jurisdictionQIDs = cells[3].split('|')
         self.country = cells[4]
         self.countryQID = cells[5]
         self.city = cells[6]
@@ -30,13 +42,29 @@ class DiplomaticRepresentation:
         self.creation = cells[20]
         self.QID = cells[21]
 
+    def title(self, include_city, include_jurisdiction):
+        title = self.type # example: 'embassy'
+        title = "%s%s" % (title[0].upper(), title[1:]) # example: 'Embassy'
+        title += ' of ' + self.operator + ' in '
+        if include_city:
+            title += self.city + ', '
+        title += self.country
+        if include_jurisdiction:
+            title += ' for ' + ', '.join(self.jurisdictions)
+
+        return title
+
     def url_name(self, include_city, include_jurisdiction):
         if (include_city):
             name = safe_for_url(self.operator) + '_in_' + safe_for_url(self.city) + '_' + safe_for_url(self.country)
         else:
             name = safe_for_url(self.operator) + '_in_' + safe_for_url(self.country)
-        if (include_jurisdiction):
-            name += ' for ' + self.jurisdiction
+        if include_jurisdiction:
+            name += '_for_' + '_'.join(self.jurisdictions)
+
+        # Truncate due to macOS default filesystem length limitation to 255 characters
+        name = name[:255 - len(".html")]
+
         return name
 
 class Operator:
@@ -79,19 +107,19 @@ index_file.write('<h1>Database of embassies and consulates</h1>\n')
 for operator in world.operators.values():
     index_file.write('<h2>Diplomatic representations operated by ' + operator.name + '</h2>\n')
     index_file.write('<ul>\n')
-    #print('operator: ' + str(operator))
+    # print('operator: ' + str(operator))
     for country in operator.countries.values():
-        #print('country: ' + str(country))
+        # print('country: ' + str(country))
         for diplo in country.values():
-            #print('diplo: ' + str(diplo))
+            # print('diplo: ' + str(diplo))
             include_city = len(country) > 1
-            include_jurisdiction = len(diplo.jurisdiction) > 1 and diplo.jurisdictionQID != diplo.countryQID
+            include_jurisdiction = len(diplo.jurisdictions) > 1 #and diplo.jurisdictionQIDs != diplo.countryQID
             filename = diplo.url_name(include_city, include_jurisdiction) + '.html'
             path = '../../../database-of-embassies.github.io/' + filename
             if (os.path.exists(path)):
                 print('Error: Several diplomatic representations for path ' + path) # TODO output both QIDs, one by parsing the HTML file
             file = open(path, 'w')
-            title = diplo.type + ' of ' + diplo.operator + ' in ' + diplo.country
+            title = diplo.title(include_city, include_jurisdiction)
             file.write('<html><title>' + title + '</title><body>\n')
             file.write('<h1>' + title + '</h1>\n')
 
@@ -103,6 +131,27 @@ for operator in world.operators.values():
                 file.write('(address:' + diplo.address + ')')
             file.write('.</p>\n')
 
+            if diplo.address:
+                file.write('<p>Address:' + diplo.address + '</p>\n')
+            if diplo.latitude and diplo.longitude:
+                file.write('<p>Map: \n')
+                file.write('<a href="https://www.openstreetmap.org/?mlat=' + diplo.latitude + '&mlon=' + diplo.longitude + '">OpenStreetMap</a>\n')
+                file.write('<a href="https://www.google.com/maps/search/?api=1&query=' + diplo.latitude + ',' + diplo.longitude + '">Google Maps</a>\n')
+                file.write('<a href="geo://' + diplo.latitude + ',' + diplo.longitude + '">geo protocol</a>\n') # Does not work on desktop browsers unfortunately
+                file.write('</p>\n')
+            if diplo.website:
+                file.write('<p><a href="' + diplo.website + '">Official website</a></p>\n')
+            if diplo.facebook:
+                file.write('<p><a href="https://www.facebook.com/' + diplo.facebook + '">Official Facebook account</a></p>\n')
+            if diplo.twitter:
+                file.write('<p><a href="https://twitter.com/' + diplo.twitter + '">Official Twitter account</a></p>\n')
+            if diplo.youtube:
+                file.write('<p><a href="https://www.youtube.com/channel/' + diplo.youtube + '">Official YouTube account</a></p>\n')
+
+            # TODO do not show image without showing its license + author + link to Commons page
+            #if diplo.picture:
+            #    file.write('<p><a href="' + diplo.picture + '"><img src="' + commons_thumbnail(diplo.picture) + '" width="300"/></a></p>\n')
+
             # TODO only if there are any
             # file.write('<p>Other diplomatic representations of ' + diplo.operator + ' in ' + diplo.country + ':\n')
             # file.write('<ul>\n')
@@ -112,12 +161,9 @@ for operator in world.operators.values():
             #     file.write('<li><a href="' + other_diplo.filename + '>' + other_diplo.city + '</a></li>\n')
             # file.write('</ul>\n')
 
-            if diplo.website:
-                file.write('<p><a href="' + diplo.website + '">Official website</a></p>\n')
-
             file.write('<p><i>Any error or missing information? Please improve <a href="' + diplo.QID + '">this Wikidata item</a>, or let us know on the <a href="https://github.com/database-of-embassies/database-of-embassies/issues">issue tracker</a>.<br/>\n')
             file.write('Public domain. Feel free to reuse this data in any way you want. Download the <a href="https://raw.githubusercontent.com/database-of-embassies/database-of-embassies/master/database_of_embassies.csv">whole CSV data</a> or browse <a href="index.html">other diplomatic representations</a>.</i></p>\n')
-            file.write('<p><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Wikidata_Stamp_Rec_Light.svg/200px-Wikidata_Stamp_Rec_Light.svg.png"/></p>\n')
+            file.write('<p><a href="' + diplo.QID + '"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Wikidata_Stamp_Rec_Light.svg/200px-Wikidata_Stamp_Rec_Light.svg.png"/></a></p>\n')
             file.write('</body></html>\n')
             file.close()
 
